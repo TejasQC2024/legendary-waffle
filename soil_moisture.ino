e959509b46b8f0dacb48e0452c668ff051db1405
TejasQC2024 <cjb16@queensu.ca>
2024-08-08 13:43:27 -0400

    Add files via upload

---
+++ soil_moisture.ino
@@ -0,0 +1,35 @@
+  // Define analog input
+  #define sensorPin A0
+  // Define the pin the speaker will be connected to 
+  int speakerPin = 2;
+  int ledPin = 13;
+
+  void setup() {
+    // Setup Serial Monitor
+    Serial.begin(9600);
+    // Allows us to send signals to the speaker
+    pinMode(speakerPin, OUTPUT);
+    pinMode(ledPin, OUTPUT);
+  }
+
+  void loop() {
+    // Read the Analog Input
+    int soilMoisture = analogRead(sensorPin);
+    // Print the moisture value to the serial monitor the higher the number the lower the moisture
+    int soilmoisturepercent = abs(map(soilMoisture, 459, 179, 0, 100));
+    Serial.print("Analog Value: ");
+    Serial.println(soilMoisture);
+    Serial.print("soilmoisturepercent: ");
+    Serial.println(soilmoisturepercent);
+    // If the moisture is too low then play a beep that continues until the moisture level is correct
+    if(soilmoisturepercent < 17){
+      tone(speakerPin, 50, 400);
+      delay(2000);
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+      digitalWrite(ledPin, LOW);
+      delay(100);
+    }
+    // Wait for 1 second before the next reading
+    delay(1000);
+  }
